1cc75c44976b6578184f9756e26e609ddf56db03
dasfex <ixodor@mail.ru>
2023-06-05 21:00:21 +0300

    Patch memlist.

--- cpp/classes/memlist/memory_list.hpp
+++ cpp/classes/memlist/memory_list.hpp
@@ -3,15 +3,13 @@
 #include <vector>
 
 // TODO In memory list и потраченное время
-// 1. iterator и переделать вставку на него.
-// 2. push back.
-// 3. push front.
-// 4. delete.
-// 5. побенчмаркать относительно обычного листа ->
+// 4. побенчмаркать относительно обычного листа ->
 // результаты сохранить куда-нибудь, чтобы потом в статью.
+// 5. delete.
 // 6. уплотнение.
 // 7. бенчмарки.
 // 8. убрать лишний индирект попробовать.
+// 9. ещё раз бенчмарки.
 
 template <typename T, std::size_t N>
 class MemList {
@@ -23,13 +21,15 @@ class MemList {
   };
 
  public:
+  struct Iterator;
+
   MemList() {
     for (std::size_t i = 0; i < N; ++i) {
       SetNgb(data_[i], i - 1, i + 1);
     }
   }
 
-  void Insert(int ind, const T& x) {  // change ind to iterator
+  void Insert(Iterator it, const T& x) {
     if (size_ == N) {
       return;
     }
@@ -49,17 +49,22 @@ class MemList {
       data_head_ = data_tail_ = node_ind;
 
       SetNgb(data_[data_head_], -1, N);
-    } else if (ind == data_head_) {
-      data_[ind].prev = node_ind;
-      data_[node_ind].next = ind;
+    } else if (it == Begin()) {
+      data_[it.ind_].prev = node_ind;
+      data_[node_ind].next = it.ind_;
 
       data_head_ = node_ind;
-    } else {
-      const int left_ind = data_[ind].prev;
-      data_[left_ind].next = node_ind;
-      data_[ind].prev = node_ind;
+    } else if (it == End()) {
+      data_[data_tail_].next = node_ind;
+      data_[node_ind].prev = data_tail_;
 
-      SetNgb(data_[node_ind], left_ind, ind);
+      data_tail_ = node_ind;
+    } else {
+      const int left_ind = data_[it.ind_].prev;
+      data_[left_ind].next = node_ind;
+      data_[it.ind_].prev = node_ind;
+
+      SetNgb(data_[node_ind], left_ind, it.ind_);
     }
 
     ++size_;
@@ -83,9 +88,61 @@ class MemList {
     return res;
   }
 
-//  void PushBack(const T& x) {
-//    Insert(data_tail_)
-//  }
+  void PushBack(const T& x) {
+    Insert(End(), x);
+  }
+
+  void PushFront(const T& x) {
+    Insert(Begin(), x);
+  }
+
+  Iterator Begin() {
+    return {this, data_head_};
+  }
+
+  Iterator End() {
+    return {};
+  }
+
+  struct Iterator {
+    Iterator(MemList<T, N>* list = nullptr, int ind = -1)
+            : list_(list), ind_(ind) {}
+
+    Iterator& operator++() {
+      if (ind_ == list_->data_tail_) {
+        list_ = nullptr;
+        ind_ = -1;
+        return *this;
+      }
+
+      ind_ = list_->data_[ind_].next;
+      return *this;
+    }
+
+    Iterator operator++() const {
+      auto tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+
+    T& operator*() {
+      return *list_->data_[ind_].value;
+    }
+
+    bool operator!=(const Iterator& rhs) const {
+      return list_ != rhs.list_ || ind_ != rhs.ind_;
+    }
+
+    bool operator==(const Iterator& rhs) const {
+      return !(*this != rhs);
+    }
+
+    friend MemList<T, N>;
+   private:
+    MemList<T, N>* list_ = nullptr;
+    int ind_ = -1;
+  };
+
 
  private:
   Node data_[N];
--- cpp/classes/memlist/tests_memory_list.cpp
+++ cpp/classes/memlist/tests_memory_list.cpp
@@ -1,24 +1,64 @@
 #include "memory_list.hpp"
 
+#include <array>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 TEST(basic, insert) {
   MemList<int, 4> l;
 
-  l.Insert(0, 12);
+  l.Insert(l.Begin(), 12);
   auto v = l.AsArray();
   ASSERT_THAT(v, ::testing::ElementsAre(12));
 
-  l.Insert(0, 13);
+  l.Insert(l.Begin(), 13);
   v = l.AsArray();
   ASSERT_THAT(v, ::testing::ElementsAre(13, 12));
 
-  l.Insert(0, 14);
-  v = l.AsArray();
-  ASSERT_THAT(v, ::testing::ElementsAre(13, 14, 12));
+  auto it = l.Begin();
+  ++it;
 
-  l.Insert(1, 14);
+  l.Insert(it, 15);
   v = l.AsArray();
-  ASSERT_THAT(v, ::testing::ElementsAre(14, 13, 14, 12));
+  ASSERT_THAT(v, ::testing::ElementsAre(13, 15, 12));
+
+  l.Insert(l.End(), 16);
+  v = l.AsArray();
+  ASSERT_THAT(v, ::testing::ElementsAre(13, 15, 12, 16));
+}
+
+TEST(basic, push_back) {
+  MemList<int, 4> l;
+  l.PushBack(1);
+  l.PushBack(2);
+  l.PushBack(3);
+  l.PushBack(4);
+  auto v = l.AsArray();
+  ASSERT_THAT(v, ::testing::ElementsAre(1, 2, 3, 4));
+}
+
+TEST(basic, push_front) {
+  MemList<int, 4> l;
+  l.PushFront(1);
+  l.PushFront(2);
+  l.PushFront(3);
+  l.PushFront(4);
+  auto v = l.AsArray();
+  ASSERT_THAT(v, ::testing::ElementsAre(4, 3, 2, 1));
+}
+
+TEST(iterators, basic) {
+  MemList<int, 4> l;
+  l.PushBack(1);
+  l.PushBack(2);
+  l.PushBack(3);
+  l.PushBack(4);
+
+  auto beg = l.Begin();
+  auto end = l.End();
+  std::array<int, 4> res = {1, 2, 3, 4};
+  for (std::size_t i = 0; beg != end; ++beg, ++i) {
+    ASSERT_EQ(*beg, res[i]);
+  }
 }
