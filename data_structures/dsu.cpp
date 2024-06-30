2785971f1d883ae1b36da1f8b11ec77be4e55457
Vanya Khodor <40736909+dasfex@users.noreply.github.com>
2024-06-30 22:30:10 +0300

    Update dsu.cpp

--- data_structures/dsu.cpp
+++ data_structures/dsu.cpp
@@ -1,20 +1,19 @@
-namespace NDsu {
-vector<int> dsu;
+class Dsu {
+  vector<int> dsu;
 
-void Init(int n) {
-    dsu.resize(n + 1, -1);
-}
+public:
+  Dsu(int n) : dsu{std::vector<int>(n + 1, -1)} {}
 
-int Find(int v) {
+  int Find(int v) {
     return dsu[v] < 0 ? v : dsu[v] = Find(dsu[v]);
-}
+  }
 
-void Union(int u, int v) {
+  void Union(int u, int v) {
     u = Find(u);
     v = Find(v);
     if (u > 0 && u == v) return;
     if (dsu[u] > dsu[v]) swap(u, v);
     dsu[u] += dsu[v];
     dsu[v] = u;
-}
-}
+  }
+};
